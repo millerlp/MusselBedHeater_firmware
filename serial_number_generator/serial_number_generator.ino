7426e7f3c251b141e53fda534a19e6fc9b0ea884
millerlp <millerlp@gmail.com>
2020-07-06 16:23:04 -0700

    new board

--- serial_number_generator/serial_number_generator.ino
+++ serial_number_generator/serial_number_generator.ino
@@ -20,7 +20,7 @@
 
 
 // serial number to write to chip. Always increment this value and save this file
-char serialnumber[]="SN50";
+char serialnumber[]="SN51";
 
 void setup(){
 	Serial.begin(57600);
